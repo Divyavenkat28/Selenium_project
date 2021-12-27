1aacc19a675dd9020cfa13c5ebc0444bd7941524
Divyavenkat28 <divyavenkat28@gmail.com>
2021-12-27 17:34:39 +0530

    code

--- src/test/java/org/test/practice.java
+++ src/test/java/org/test/practice.java
@@ -1,5 +1,9 @@
 package org.test;
 
 public class practice {
-
+	public static void main(String[] args) {
+		System.out.println("hi");
+		System.out.println("good morning");
+		System.out.println("good Evening");
+	}
 }
